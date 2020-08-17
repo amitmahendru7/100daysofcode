364fe4c83e87cee1e453084e498f06799dfe8fb7
amitm <amitm@10.0.0.104>
2020-08-16 23:04:52 -0400

    Day 5 of #100DaysofCode- Continuing with arrays-Medium Leetcode

---
+++ 100DaysOfCode/day5/targetArrayInGivenOrder/Solution.java
@@ -0,0 +1,29 @@
+package targetArrayInGivenOrder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		int[] nums= {0,1,2,3,4};
+		int[] index= {0,1,2,2,1};
+		System.out.println(Arrays.toString(createTargetArray(nums, index)));
+
+	}
+
+	 public static int[] createTargetArray(int[] nums, int[] index) {
+         ArrayList<Integer> a = new ArrayList<Integer>();
+        for(int i=0;i<nums.length;i++)
+        {
+            a.add(index[i],nums[i]);
+        }
+        int target[] = new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            target[i] = a.get(i);
+        }
+        return target;
+        
+    }
+}
