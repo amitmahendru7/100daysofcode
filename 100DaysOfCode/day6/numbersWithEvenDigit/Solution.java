0ec13073dffbbfcf9d0d8b0681b44cf689833684
amitm <amitm@10.0.0.104>
2020-08-17 23:48:30 -0400

    Day 6, Wisdom Tooth pain and yet here I code

---
+++ 100DaysOfCode/day6/numbersWithEvenDigit/Solution.java
@@ -0,0 +1,27 @@
+package numbersWithEvenDigit;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		int[] arr= {12,345,2,6,7896,100000};
+		System.out.println(findNumbers(arr));
+
+	}
+	
+	 public static int findNumbers(int[] nums) {
+	        //Edge Case
+	        if(nums.length==0)
+	            return 0;
+	        int counter=0;
+	        for(int i=0;i<nums.length;i++){
+	            if((nums[i]>9 && nums[i]<100) || (nums[i]>999 && nums[i]<10000) || (nums[i]==100000)){
+	                counter++;
+	            }
+	        }
+	        return counter;
+	            
+	      
+	        
+	    }
+
+}
