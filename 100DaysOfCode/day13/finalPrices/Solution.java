8aa11329b9e07e5cf469488c71df2689ee5ed11d
amitm <amitm@10.0.0.104>
2020-08-26 22:59:16 -0400

    Day 13

--- 100DaysOfCode/day13/finalPrices/Solution.java
+++ 100DaysOfCode/day13/finalPrices/Solution.java
@@ -1,10 +1,24 @@
 package finalPrices;
 
+import java.util.Stack;
+
 public class Solution {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 
 	}
+	
+	 public static int[] finalPrices(int[] prices) {
+	        Stack<Integer> stack = new Stack<>();
+	        for (int i = 0; i < prices.length; i++) {
+	            while (!stack.isEmpty() && prices[stack.peek()] >= prices[i])
+	                prices[stack.pop()] -= prices[i];
+	            stack.push(i);
+	        }
+	        return prices;
+	        
+	    }
 
 }
